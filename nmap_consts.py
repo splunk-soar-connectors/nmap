d804321529a9b182be2baab9a37acfcfe80ca88b
Atif Mahadik <atif@phantom.us>
2017-08-22 13:45:29 -0700

    Revert "PAPP-3026"
    This reverts commit a18f9f32753c9ce0c7e12674f0c1dba416524393.

--- nmap_consts.py
+++ nmap_consts.py
@@ -14,6 +14,9 @@
 
 NMAP_JSON_IP_HOSTNAME = "ip_hostname"
 NMAP_JSON_PORTLIST = "portlist"
+NMAP_JSON_UDP = "udp_scan"
+NMAP_JSON_SCRIPT = "script"
+NMAP_JSON_SCRIPT_ARGS = "script-args"
 NMAP_ERR_SCAN = "nmap scan failed"
 NMAP_SUCC_SCAN = "nmap scanesuccessful"
 NMAP_ERR_SCAN_RETURNED_NO_DATA = "nmap scan did not return any information"
