3e5ebeaeb1af8c5d868db18955f3d1dbb23480b5
Dushyant Parmar <dushyantp@splunk.com>
2021-01-04 16:58:02 +0530

    PAPP-13187 updates license acc. to template

--- nmap_consts.py
+++ nmap_consts.py
@@ -1,11 +1,8 @@
-# --
 # File: nmap_consts.py
 # Copyright (c) 2016-2021 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
-#
-# --
 
 NMAP_JSON_IP_HOSTNAME = "ip_hostname"
 NMAP_JSON_PORTLIST = "portlist"
--- nmap_view.py
+++ nmap_view.py
@@ -1,11 +1,9 @@
-# --
 # File: nmap_view.py
 # Copyright (c) 2016-2021 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
-#
-# --
+
 
 import phantom.utils as ph_utils
 
