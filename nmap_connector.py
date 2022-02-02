07bc91da124c0bec27b4e792ed62d3433840637e
Mary Pan <mpan@splunk.com>
2022-02-02 15:57:32 -0800

    add support for python3.9

--- nmap_connector.py
+++ nmap_connector.py
@@ -281,7 +281,7 @@ if __name__ == '__main__':
 
     if (len(sys.argv) < 2):
         print("No test json specified as input")
-        exit(0)
+        sys.exit(0)
 
     with open(sys.argv[1]) as f:
         in_json = f.read()
@@ -293,4 +293,4 @@ if __name__ == '__main__':
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(ret_val)
 
-    exit(0)
+    sys.exit(0)
--- release_notes/unreleased.md
+++ release_notes/unreleased.md
@@ -1,1 +1,3 @@
 **Unreleased**
+* Marked the app as FIPS Compliant [PAPP-21705]
+* Added support for Python 3.9
